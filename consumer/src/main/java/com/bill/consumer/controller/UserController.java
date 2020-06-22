8edca2b9d0ff9ef26ad44878ab8f99cd59757af5
BillLin1209 <zzx123bill@gmail.com>
2020-06-23 00:00:21 +0800

    consumer properties setting

--- consumer/src/main/java/com/bill/consumer/controller/UserController.java
+++ consumer/src/main/java/com/bill/consumer/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.bill.consumer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
+	@GetMapping("/buy")
+	public String buyTicket(String name) {
+		return name+"買了"+"";
+	}
 }
